5875893daf071ecff056d64e54d10a442f9ae433
TejashriMahajan <tejmahajan11@gmail.com>
2026-06-28 23:22:15 +0530

    Fixed pom and imports

--- src/main/java/ecommerce/ObjectRepo/LoginPage.java
+++ src/main/java/ecommerce/ObjectRepo/LoginPage.java
@@ -1,46 +1,16 @@
 package ecommerce.ObjectRepo;
 
-import java.sql.Driver;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.PageFactory;
-//
-//public class LoginPage {
-//	WebDriver driver;
-//	
-//	public LoginPage(WebDriver driver)
-//	{
-//		this.driver=driver;
-//		PageFactory.initElements(driver,this);
-//	}
-//	
-//	@FindBy(xpath="//a[normalize-space()='Log in']")
-//	public WebElement Loginlink;
-//	
-//	@FindBy(id="Email")
-//	public WebElement Email;
-//	
-//	@FindBy(id="Password")
-//	public WebElement Password;
-//	
-//	@FindBy(xpath="//input[@value='Log in']")
-//	public WebElement Loginbutton;
-//	
-//	@FindBy(xpath="//a[normalize-space()='Log out']")
-//	public WebElement logoutLink;
-//	
-//	
-//	
-//}
 
-public class LoginPage{
+
+
+public class LoginPage {
 	WebDriver driver;
 	
 	public LoginPage(WebDriver driver)
@@ -48,7 +18,25 @@ public class LoginPage{
 		this.driver=driver;
 		PageFactory.initElements(driver,this);
 	}
-	@FindBy("")
-	public WebElement login;
+	
+	@FindBy(xpath="//a[normalize-space()='Log in']")
+	public WebElement Loginlink;
+	
+	@FindBy(id="Email")
+	public WebElement Email;
+	
+	@FindBy(id="Password")
+	public WebElement Password;
+	
+	@FindBy(xpath="//input[@value='Log in']")
+	public WebElement Loginbutton;
+	
+	@FindBy(xpath="//a[normalize-space()='Log out']")
+	public WebElement logoutLink;
+	
+	
 	
 }
+
+
+	
